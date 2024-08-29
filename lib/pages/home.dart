827bbdd89e500ad4a12ad58c72a73a6d2de7aabd
waiyanphoneaant <wypa07@gmail.com>
2024-08-29 12:20:51 +0700

    category section

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,94 +1,189 @@
+import 'package:fitness/models/category_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class HomePage extends StatelessWidget {
-const  HomePage({ super.key }) ;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context){
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<CategoryModel> categories = [];
+
+  void _getCategories() {
+    categories = CategoryModel.getCategories();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _getCategories();
     return Scaffold(
       appBar: appBar(),
-      body: Column( 
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            margin:const EdgeInsets.only(top:40,left:20,right:20),
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0xff1D1617).withOpacity(0.11),
-                  blurRadius: 40,
-                  spreadRadius: 0.00,
-                )
-              ]
-            ),
-            child:TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                border:OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  borderSide: BorderSide.none
-                )
+          __searchField(),
+          const SizedBox(
+            height: 40,
+          ),
+          _categoriesSection()
+        ],
+      ),
+    );
+  }
+
+  Column _categoriesSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Text(
+            'Category',
+            style: TextStyle(
+                color: Colors.black, fontSize: 18, fontWeight: FontWeight.w600),
+          ),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Container(
+          height: 120,
+          // color: Colors.green,
+          child: ListView.separated(
+              itemCount: categories.length,
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              separatorBuilder: (context, index) => const SizedBox(width: 25),
+              itemBuilder: (context, index) {
+                return Container(
+                  width: 100,
+                  decoration: BoxDecoration(
+                      color: categories[index].boxColor.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                            color: Colors.white, shape: BoxShape.circle),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SvgPicture.asset(categories[index].iconPath),
+                        ),
+                      ),
+                      Text(
+                        categories[index].name,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black,
+                            fontSize: 14),
+                      )
+                    ],
+                  ),
+                );
+              }),
+        )
+      ],
+    );
+  }
+
+  Container __searchField() {
+    return Container(
+      margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: const Color(0xff1D1617).withOpacity(0.11),
+          blurRadius: 40,
+          spreadRadius: 0.00,
+        )
+      ]),
+      child: TextField(
+        decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            hintStyle: const TextStyle(color: Color(0xffDDDADA), fontSize: 14),
+            hintText: "Search PanCake",
+            contentPadding: const EdgeInsets.all(15),
+            prefixIcon: Padding(
+              padding: const EdgeInsets.all(12),
+              child: SvgPicture.asset(
+                'assets/icons/Search.svg',
               ),
             ),
-          )
-        ],
+            suffixIcon: Container(
+              width: 100,
+              child: IntrinsicHeight(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const VerticalDivider(
+                      color: Color(0xffDDDADA),
+                      indent: 10,
+                      endIndent: 10,
+                      thickness: 0.1,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: SvgPicture.asset(
+                        'assets/icons/Filter.svg',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none)),
       ),
     );
   }
 
   AppBar appBar() {
     return AppBar(
-      title:const Text('Welcome',
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 18,
-          fontWeight: FontWeight.bold
-        )
-      ),
+      title: const Text('Welcome ',
+          style: TextStyle(
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold)),
       backgroundColor: Colors.white,
       centerTitle: true,
-      elevation: 0.0,  // means shadows
-      leading: 
-      GestureDetector(
-        onTap: (){
-
-        },
-        child:Container(
+      elevation: 0.0, // means shadows
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
           alignment: Alignment.center,
-          margin:const EdgeInsets.all(10),
-          decoration:BoxDecoration(
-            color:const Color(0xffF7F8F8),
-            borderRadius: BorderRadius.circular(10)
-          ),
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              color: const Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10)),
           child: SvgPicture.asset(
             'assets/icons/Arrow - Left 2.svg',
             height: 20,
             width: 20,
-              
-            ),
+          ),
         ),
       ),
       actions: [
         GestureDetector(
-          onTap: (){
-
-          },
-          child:Container(
-            alignment: Alignment.center,
-            width: 37,
-            margin:const EdgeInsets.all(10),
-            decoration:BoxDecoration(
-              color:const Color(0xffF7F8F8),
-              borderRadius: BorderRadius.circular(10)
-            ),
-            child: SvgPicture.asset(
-              'assets/icons/dots.svg',
-              height: 5,
-              width: 5,
-                
+            onTap: () {},
+            child: Container(
+              alignment: Alignment.center,
+              width: 37,
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  color: const Color(0xffF7F8F8),
+                  borderRadius: BorderRadius.circular(10)),
+              child: SvgPicture.asset(
+                'assets/icons/dots.svg',
+                height: 5,
+                width: 5,
               ),
-          ))
+            ))
       ],
     );
   }
